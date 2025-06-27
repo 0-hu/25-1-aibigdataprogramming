cde5f4c392282532899e94a401765468a123ead0
YoungHu Kwon <166782530+0-hu@users.noreply.github.com>
2025-06-27 16:18:28 +0900

    Add files via upload
    add 4week mission

---
+++ 문제3.py
@@ -0,0 +1,6 @@
+t=int(input('정수 입력:'))
+
+print('\n%d%d%d%d%d%d' % (t,t,t,t,t,t))
+print('%d    %d' % (t,t))
+print('%d    %d' % (t,t))
+print('%d%d%d%d%d%d' % (t,t,t,t,t,t))
---
+++ 문제6.py
@@ -0,0 +1,7 @@
+n1=int(input('숫자1 입력:'))
+n2=int(input('숫자2 입력:'))
+n3=int(input('숫자3 입력:'))
+
+print("\n")
+print("%d + %d + %d = %d" % (n1, n2, n3, n1+n2+n3))
+print("%d * %d * %d = %d" % (n1, n2, n3, n1*n2*n3))
