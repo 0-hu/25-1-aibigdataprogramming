72578707a392e545e948273e155c71d917de4144
0-hu <yhkwon.net@gmail.com>
2025-07-02 17:43:57 +0900

    add 6&7week

---
+++ 7week/7-문제1.py
@@ -0,0 +1,9 @@
+x = int(input('정수를 입력하시오:'))
+print(x, '은/는 3의 배수') if x%3==0 else print(x,'은/는 3의 배수 아님')
+
+
+# x = int(input('정수를 입력하시오:'))
+# if x%3 == 0:
+#     print(x, '은/는 3의 배수')
+# else:
+#     print(x,'은/는 3의 배수 아님')
---
+++ 7week/7-연산자 문제-3.py
@@ -0,0 +1,7 @@
+n=int(input())
+n+=500       #;print(n)      # n=36300
+n//=1000     #;print(n)      # n=36
+n*=1000      #;print(n)      # n=36000
+print("%d" %n)
+
+# print("%d" % ((n+500)//1000*1000))
